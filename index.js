dbbd5e44fd164638ff805108ce3987cec4f5e8f3
cheskoxd <franchesko77lacayo@gmail.com>
2025-05-17 02:28:54 -0500

    add .env file for CORS configuration and update index.js to use environment variable

--- index.js
+++ index.js
@@ -2,12 +2,13 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+require('dotenv').config()
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-  origin: "http://localhost:5173",
+  origin: process.env.CORS_ORIGIN || "http://localhost:3000",
   methods: ["GET", "POST"],
   credentials: true,
 }
